29f6469b01c4195a45710c3c7c9b3e513bc7002a
SEC-MASUM <masum.jess42@gmail.com>
2022-04-17 08:55:50 +0600

    revert  : .env.local is not working it showing error

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -2,17 +2,24 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  //   apiKey: process.env.REACT_APP_apiKey,
+  //   authDomain: process.env.REACT_APP_authDomain,
+  //   projectId: process.env.REACT_APP_projectId,
+  //   storageBucket: process.env.REACT_APP_storageBucket,
+  //   messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  //   appId: process.env.REACT_APP_appId,
+  //   -----------------------------
+  apiKey: "AIzaSyAHsWfDWa7ZDSugGDo_0vPJ6nyhXO4HsoM",
+  authDomain: "tutor-guru-01.firebaseapp.com",
+  projectId: "tutor-guru-01",
+  storageBucket: "tutor-guru-01.appspot.com",
+  messagingSenderId: "459360069002",
+  appId: "1:459360069002:web:925dbc33a9e4dc78cd1473",
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(firebaseApp);
+const auth = getAuth(app);
 
 export default auth;
