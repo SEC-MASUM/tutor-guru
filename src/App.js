9e665d65c9f984304aa2fd83423b914973945db4
SEC-MASUM <masum.jess42@gmail.com>
2022-04-17 06:46:24 +0600

    feat : create a bunch of components and set routes for Home NotFound

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
-    <div >
+    <div>
       <h1>Tutor Guru</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
