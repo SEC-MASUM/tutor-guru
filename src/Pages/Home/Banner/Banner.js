cf38eef0da1542ced8e04c96fe66fe4f64e2f6ef
SEC-MASUM <masum.jess42@gmail.com>
2022-04-18 23:23:51 +0600

    style: Banner responsive

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,21 +5,19 @@ import bottomPattern from "../../../Assets/images/download/slider-bottom-pattern
 
 const Banner = () => {
   return (
-    <div className="relative justify-center items-center  h-[800px] bg-yellow-400 ">
-      <div className="container  grid grid-cols-2 h-full  mx-auto">
-        <div className=" relative">
-          <img
-            src={person}
-            alt="tutor-photo"
-            className="h-[700px] absolute bottom-0"
-          />
+    <div className="relative justify-center items-center   bg-yellow-400 ">
+      <div className="container   grid grid-cols-1 md:grid-cols-2  mx-auto">
+        <div className="  order-2 md:order-1">
+          <img src={person} alt="tutorPhoto" />
         </div>
 
-        <div className="w-full my-auto">
-          <h1 className="inline-block bg-white text-rose-600 font-bold text-6xl px-6 py-3">
+        <div className="  order-1 md:order-2 py-10 my-auto mx-auto">
+          <h1 className=" text-rose-600 font-bold text-center md:text-left  text-6xl px-6 py-3 ">
             Professional Tutor
           </h1>
-          <h1 className="text-gray-800 font-bold text-5xl px-6 py-3">For Your Children</h1>
+          <h1 className=" font-bold text-center md:text-left text-5xl px-6 py-3">
+            For Your Children
+          </h1>
         </div>
       </div>
       <img src={bottomPattern} alt="" className="w-full absolute bottom-0" />
